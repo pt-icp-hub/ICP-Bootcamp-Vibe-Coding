bb4e208a721d1aa25ad1af6b5e4e109f49742ddc
Antoine Espieux <antoine.espieux@hotmail.fr>
2025-04-24 17:02:11 +0200

    devcontainer: install jq

--- scripts/dev-container-setup.sh
+++ scripts/dev-container-setup.sh
@@ -3,4 +3,6 @@
 dfx identity new codespace_dev  --storage-mode=plaintext     
 dfx identity use codespace_dev      
 dfx start --background             
-dfx stop         
+dfx stop
+
+apt-get update && apt-get install -y jq
